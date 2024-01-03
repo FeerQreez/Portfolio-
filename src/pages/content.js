2e730f2948ecf66db6a049f24f1a64b9f557ad96
¨fernando¨ <feerqreez@gmail.com>
2024-01-03 16:42:19 -0600

    first commit - test

---
+++ src/pages/content.js
@@ -0,0 +1,156 @@
+// Personal Information --> #hero section
+export const name = "Fernando Campos "
+export const designation = "Sotfware Developer Fullstack"
+export const location = "Edo.Mex , CDMX"
+export const pronouns = "Github"
+export const website = "https://github.com/FeerQreez"
+
+// About
+export const about = "  Soy una persona proactiva, organizada y responsable, con buenas relaciones interpersonales. Siempre tengo la mejor disposición para la realización de mis labores. Busco un puesto de trabajo desafiante donde pueda continuar aprendiendo, y a la vez compartir mi experiencia y conocimientos con el equipo de trabajo."
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "2022",
+		to: "2023",
+		role: "Web Fullstack Jr Advanced ",
+		org: "CCAI-TESE , Ihualia , Dayser Consultores",
+		org_url: "https://ccaitese.com/",
+		location: "Avenida Tecnologico , Valle de Anahuac Ecatepec de Morelos S/N ",
+		skills: "Desarrollo Frontend, Desarrollo Backend, Api Rest Slim framework, SQL Databases, CRUD to 16 endpoints "
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "Tesla Landing Page",
+		description: "el proyecto esta creado en Astro creandola landing page de tesla siguiendo el tutorial de midudev.",
+		url: "https://github.com/FeerQreez/Tesla-Landing-Page",
+		tech: "Astro Javascript Framework , Training Programs, html, tailwind css",
+	},
+	{
+		title: "Sistema de generacion de alertas para el cumplimiento de la norma 035 de la STPS ",
+		description: "Este proyecto lo hice en mi escuela como practicas profesionales, para el cumplimiento de la norma 035 para la secretaria de trabajo y previnsion social.",
+		url: "https://github.com/FeerQreez/Norma035-STPS",
+		tech: "training programs, Educational Material, norma 035 , Tecnologico ",
+	},
+	{
+		title: "Inventario App en React Native",
+		description: " Esta es una app movil creada en react native como practica para recrear el almacen de tipo sam´s club o costo.",
+		url: "https://github.com/FeerQreez/InventarioAppFer-",
+		tech: "App Movile, React native, training porgram ",
+	}
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2016",
+		to: "2023",
+		institute: "Tecnologico de Estudios Superiores de Ecatepec ",
+		course: "Ingenieria en Sistemas Computacionales",
+		location: "Av tecnologico , valle de Anahuac Edomex Ecatepec de Morelos"
+	},
+	{
+	//Espacio editable	
+	},
+	{
+		from: "2021",
+		to: "2023",
+		institute: "Cisco Network Academy",
+		course: "CCNA 2  Protocolos y Conceptos de Ruteo Módulo 2: Fundamentos de Routing, Switching y Wireless",
+		location: "NetAcademy"
+	}
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		source_name: "Email",
+		source: "@gmail.com",
+	},
+	{
+		source_name: "Twitter",
+		source: "https://twitter.com/",
+	},
+	{
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/fernando-campos-383b81123/",
+	},
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+		issued: "2022",
+		title: "Certificaciones de CCNA  modulos CCNAv7: Redes empresariales, Seguridad y Automatización",
+		org: "Cisco Network Academy",
+		url: "https://",
+		description: " Redes empresariales, Seguridad y Automatización Configurar OSPFv2 de área única en redes punto a punto ",
+	},
+	
+	{
+		issued: "2021",
+		title: "CCNA Routing and Switching: Principios básicos de routing y switching ",
+		org: "espacio libre",
+		url: "espacio libre",
+		description: "Explicar cómo mitigar las amenazas y mejorar la seguridad de la red utilizando listas de control de acceso y mejores prácticas de segurida.",
+	},
+	{
+		issued: "2023",
+		title: "espacio libre",
+		org: "espacio libre",
+		url: "https://",
+		description: "Explicar cómo optimizar, supervisar y solucionar problemas de arquitecturas de red escalable.",
+	}
+]
+
+// Blogs --> #blogs section
+export const blogs = [
+	{
+		date: "20-Febrero, 2023",
+		url: "https://revista.ccaitese.com/index.php/ridt/article/view/4",
+		title: "El Sistema De generacion de alertas para el Cumplimiento de la nom-035 de la STPS ",
+		description: "Aqui tenemos publicado el articulo en la revista cientifica de mi institucion Educativa  ",
+		publisher: "RICT (Revista de inovacion cientifica, tecnologica y investigacion)",
+	},
+	{
+		date: "25-June, 2023",
+		url: "Espacio Libre ",
+		title: "Espacio libre ",
+		description: "Espacio libre ",
+		publisher: "Espacio Libre",
+	}
+]
+
+// Achievements --> #achievement section
+export const achievements = [
+	{
+		title: "Voluntario talent Network ",
+		year: "2019",
+		decription: "asisti al Papalote museo del niño para enseñar programacion a niños pequeños mediante la tecnologia de MakeBlock  for Creativa Kids."
+	},
+	{
+		title: " Hackathon Bancomer ",
+		year: "2018",
+		decription: "asisti al hackathon BBVA en la torre bancomer el cual tenia varios retos y en el cual me integre a un equipo proponiendo solucionar una problemantica mendiante nuestra app power change ."
+	},
+	{
+		title: "Voluntario Talent Network",
+		year: "2018",
+		decription: "Participe en un voluntariado checando el area de Streaming sobre un evento de Tecnologia ."
+	},
+	{
+		title: "Asistir a Campus Party ",
+		year: "2018",
+		decription: "Asisti a talleres de blockchain , cyberseguridad y robotica durante el evento me diverti y conoci de videojuegos  nuevos  como simuladores de VR y simuladores de carreras "
+	},
+	{
+		title: "Concursar en la semana del emprendedor ",
+		year: "2017",
+		decription: "gane el 3er lugar a la mejor idea  en la semana del emprendedor ."
+	}
+	
+]
